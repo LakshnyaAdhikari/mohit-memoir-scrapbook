bb2088e928c33d107ba2254d01b3a1f8880c4435
LakshnyaAdhikari <lakshnyaadhikari1@gmail.com>
2026-06-14 02:57:41 +0530

    basepath for GitHub Pages routing

--- src/router.tsx
+++ src/router.tsx
@@ -7,6 +7,7 @@ export const getRouter = () => {
 
   const router = createRouter({
     routeTree,
+    basepath: "/mohit-memoir-scrapbook",
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
